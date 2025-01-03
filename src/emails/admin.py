886a606eb6505c3e249197637679c7e2ab549029
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-01-03 16:51:39 +0300

    email verification logic

--- src/emails/admin.py
+++ src/emails/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Email, EmailVerificationEvent
 
 # Register your models here.
+admin.site.register(Email)
+admin.site.register(EmailVerificationEvent)
--- src/emails/forms.py
+++ src/emails/forms.py
@@ -1,8 +1,25 @@
 from django import forms
+from . import css 
 from .models import Email
+# from .models import Email, EmailVerificationEvent
 
-class EmailForm(forms.ModelForm):
+class EmailForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs = {
+                "id": "email-login-input",
+                "class": css.EMAIL_FIELD_CSS,
+                "placeholder": "your email login"
+            }
+        )
+    )
+    # class Meta:
+    #     model = EmailVerificationEvent
+    #     fields = ['email']
 
-    class Meta:
-        model = Email
-        fields = ['email']
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        qs = Email.objects.filter(email=email, active=False)
+        if qs.exists():
+            raise forms.ValidationError("Invalid email.Please try again")
+        return email
